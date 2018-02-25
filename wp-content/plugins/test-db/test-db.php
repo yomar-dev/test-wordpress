2566f0387561581999ea59cf7d4d305011c0c317
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 09:07:10 -0500

    Creación de la cabecera de nuestro plugin.

---
+++ wp-content/plugins/test-db/test-db.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Test DB
+* Plugin URI: https://github.com/yomar-dev/test-wordpress
+* Description: Este plugin crea una tabla en la BD de wordpress
+* Version: 1.0.0
+* Author: Yomar Miranda
+* Author URI: https://yomar-dev.github.io/
+* License: GPL2
+*/
