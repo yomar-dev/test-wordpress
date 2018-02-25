f3f8a2b386a1ba98d1ec628769e39c474bd07d56
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 11:55:18 -0500

    Conexión a la BD.

---
+++ register/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+$dbHost = "localhost";
+$dbName = "pruebawordpress";
+$dbUser = "root";
+$dbPass = "root";
+
+try{
+	$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(Exception $e){
+	echo $e->getMessage();
+}
+
+?>
